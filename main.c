fabc12ffd867c8d4087cf711ce0878d5c30e21cf
1nzag <wkdwns420@gmail.com>
2017-07-31 22:38:28 -0400

    Last upload

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
 	char errbuf[PCAP_ERRBUF_SIZE];
 	uint8_t MAC_addr[6] = {0,};
 	uint8_t senders_MAC[6] = {0,};
+	uint8_t broadcast_MAC[6] = {0xff,0xff,0xff,0xff,0xff,0xff};
 	struct in_addr my_IP;
 	struct in_addr sender_ip;
 	struct in_addr target_ip;
@@ -19,14 +20,17 @@ int main(int argc, char* argv[])
 	*/
 	inet_aton(argv[2],&sender_ip);
 	inet_aton(argv[3],&target_ip);
-	printf("sender ip : %x\n", sender_ip);
+
 	handle = pcap_open_live(argv[1],BUFSIZ,1,1000,errbuf);
 	
 	get_addr(MAC_addr,&my_IP ,argv[1]);
-	rs_ARP(handle,MAC_addr,&my_IP , &sender_ip,1); // request mode
 
-	//get_senders_mac(sender, sender_ip, senders_MAC);
+	printf("%s\n",inet_ntoa(my_IP));
+	rs_ARP(handle,MAC_addr,broadcast_MAC,&my_IP , &sender_ip,1); // request mode
 
-	rs_ARP(handle, MAC_addr, &target_ip, &sender_ip, 0); //send mode
+	get_senders_mac(sender, sender_ip, senders_MAC);
+
+	rs_ARP(handle,MAC_addr,senders_MAC ,&target_ip, &sender_ip, 0); //send mode
+	close(handle);
 	return 0;
 }
